869a1730c9f826808330c76ab253c19d6b329f2d
Gearnode <bfrimin@student.42.fr>
2016-06-12 17:22:59 +0200

    Comment dev code

--- config/dev.exs
+++ config/dev.exs
@@ -1,5 +1,5 @@
 use Mix.Config
-config :stripe_eventex, api_key: "stripe_api_key"
-config :stripe_eventex, subscibed_events: [
-	{"customer.created", Stripe.CustomerCreated},
-	{"customer.updated", Stripe.CustomerUpdated} ]
+# config :stripe_eventex, api_key: "stripe_api_key"
+# config :stripe_eventex, subscibed_events: [
+# 	{"customer.created", Stripe.CustomerCreated},
+# 	{"customer.updated", Stripe.CustomerUpdated} ]
