b96492a18ac7e233d21990db2c5dddd09cd9e37e
Gearnode <bfrimin@student.42.fr>
2016-06-12 15:45:14 +0200

    Style

--- test/stripe_eventex_test.exs
+++ test/stripe_eventex_test.exs
@@ -1,48 +1,48 @@
 # {:ok, _} = Plug.Adapters.Cowboy.http StripeEventex, [log: true, verify_event: true, path: "/"]
 defmodule Stripe.CustomerCreated do
-	def perform(_opts) do
-	end
+  def perform(_opts) do
+  end
 end
 
 # plug StripeEventex, log: true, verify_event: true, path: "/"
 
 defmodule StripeEventexTest do
-	use ExUnit.Case, async: true
-	use Plug.Test
+  use ExUnit.Case, async: true
+  use Plug.Test
 
-	@opts StripeEventex.init([path: "/"])
+  @opts StripeEventex.init([path: "/"])
 
-	test "when path arguments is not present" do
-		assert_raise ArgumentError, fn ->
-			conn = StripeEventex.call(conn(:post, "/", Poison.encode!(%{event: "unknown"})), StripeEventex.init([]))
-		end
-	end
+  test "when path arguments is not present" do
+    assert_raise ArgumentError, fn ->
+      conn = StripeEventex.call(conn(:post, "/", Poison.encode!(%{event: "unknown"})), StripeEventex.init([]))
+    end
+  end
 
-	test "when unknown event, 200 must be returned" do
-		conn = StripeEventex.call(conn(:post, "/", Poison.encode!(%{event: "unknown"})), @opts)
-		assert conn.status == 200
-		assert get_resp_header(conn, "content-type") == ["application/json; charset=utf-8"]
-		assert conn.resp_body == Poison.encode!(%{message: "success (not subscribed)"})
-	end
+  test "when unknown event, 200 must be returned" do
+    conn = StripeEventex.call(conn(:post, "/", Poison.encode!(%{event: "unknown"})), @opts)
+    assert conn.status == 200
+    assert get_resp_header(conn, "content-type") == ["application/json; charset=utf-8"]
+    assert conn.resp_body == Poison.encode!(%{message: "success (not subscribed)"})
+  end
 
-	test "when payload is empty, 200 must be returned" do
-		conn = StripeEventex.call(conn(:post, "/", Poison.encode!(%{})), @opts)
-		assert conn.status == 200
-		assert get_resp_header(conn, "content-type") == ["application/json; charset=utf-8"]
-		assert conn.resp_body == Poison.encode!(%{message: "success (not subscribed)"})
-	end
+  test "when payload is empty, 200 must be returned" do
+    conn = StripeEventex.call(conn(:post, "/", Poison.encode!(%{})), @opts)
+    assert conn.status == 200
+    assert get_resp_header(conn, "content-type") == ["application/json; charset=utf-8"]
+    assert conn.resp_body == Poison.encode!(%{message: "success (not subscribed)"})
+  end
 
-	test "when subscribed event fail" do
-		conn = conn(:post, "/", Poison.encode!(%{event: "customer.updated"}))
-		assert_raise StripeEventex.MissingStripeEventModule, fn ->
-			conn = StripeEventex.call(conn, @opts)
-		end
-	end
+  test "when subscribed event fail" do
+    conn = conn(:post, "/", Poison.encode!(%{event: "customer.updated"}))
+    assert_raise StripeEventex.MissingStripeEventModule, fn ->
+      conn = StripeEventex.call(conn, @opts)
+    end
+  end
 
-	test "when subcribed event " do
-		conn = StripeEventex.call(conn(:post, "/", Poison.encode!(%{event: "customer.created"})), @opts)
-		assert conn.status == 200
-		assert get_resp_header(conn, "content-type") == ["application/json; charset=utf-8"]
-		assert conn.resp_body == Poison.encode!(%{message: "success"})
-	end
+  test "when subcribed event " do
+    conn = StripeEventex.call(conn(:post, "/", Poison.encode!(%{event: "customer.created"})), @opts)
+    assert conn.status == 200
+    assert get_resp_header(conn, "content-type") == ["application/json; charset=utf-8"]
+    assert conn.resp_body == Poison.encode!(%{message: "success"})
+  end
 end
