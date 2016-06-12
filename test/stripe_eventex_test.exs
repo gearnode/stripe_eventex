44ff901278f3ec4a3acf6b3421ffd299a7ac0cef
Gearnode <bfrimin@student.42.fr>
2016-06-12 13:00:06 +0200

    Add spec when is fail event

--- test/stripe_eventex_test.exs
+++ test/stripe_eventex_test.exs
@@ -1,9 +1,27 @@
 defmodule HelloTest do
 defmodule StripeEventexTest do
-  use ExUnit.Case, async: true
-  use Plug.Test
+	use ExUnit.Case, async: true
+	use Plug.Test
 
-  @opts StripeEventex.init([])
+	@opts StripeEventex.init([])
+
+	test "when unknown event 200 must be returned" do
+		conn = StripeEventex.call(conn(:post, "/", Poison.encode!(%{event: "unknown"})), @opts)
+		assert conn.status == 200
+		assert get_resp_header(conn, "content-type") == ["application/json; charset=utf-8"]
+		assert conn.resp_body == Poison.encode!(%{message: "success (not subscribed)"})
+	end
+
+	test "when is subscribed event fail" do
+		conn = conn(:post, "/", Poison.encode!(%{event: "customer.updated"}))
+		assert_raise StripeEventex.MissingStripeEventModule, fn ->
+			conn = StripeEventex.call(conn, @opts)
+		end
+	end
+
+	test "" do
+
+	end
 
   test "when is unknown event" do
   	conn = StripeEventex.call(conn(:post, "/", Poison.encode!(%{event: "customer.jshj"})), @opts)
