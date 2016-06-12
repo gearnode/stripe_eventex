3248b58b2ae49197a990be713c40b39dc90a909e
Gearnode <bfrimin@student.42.fr>
2016-06-12 13:00:44 +0200

    Style

--- test/stripe_eventex_test.exs
+++ test/stripe_eventex_test.exs
@@ -23,15 +23,9 @@ defmodule StripeEventexTest do
 
 	end
 
-  test "when is unknown event" do
-  	conn = StripeEventex.call(conn(:post, "/", Poison.encode!(%{event: "customer.jshj"})), @opts)
-  	assert conn.status == 200
-  	assert get_resp_header(conn, "content-type") == ["application/json; charset=utf-8"]
-  	assert conn.resp_body == Poison.encode!(%{message: "success (not subscribed)"})
-  end
 
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
+	test "the truth" do
+		assert 1 + 1 == 2
+	end
 end
