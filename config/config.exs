a0d3cc6584b580e9a3c01861a6b4903f5810177d
Gearnode <bfrimin@student.42.fr>
2016-06-12 13:09:09 +0200

    Use environment for app configuration

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,2 @@
 use Mix.Config
-
-config :stripe_eventex, api_key: "stripe_api_key"
-config :stripe_eventex, subscibed_events: [
-	{"customer.created", Stripe.CustomerCreated},
-	{"customer.updated", Stripe.CustomerUpdated} ]
+import_config "#{Mix.env}.exs"
