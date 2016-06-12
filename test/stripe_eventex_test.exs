8cccf5e1035a74cfb581b250eb09d9ce2861ec0e
Gearnode <bfrimin@student.42.fr>
2016-06-12 12:44:15 +0200

    Add spec

--- test/stripe_eventex_test.exs
+++ test/stripe_eventex_test.exs
@@ -1,5 +1,16 @@
 defmodule HelloTest do
+defmodule StripeEventexTest do
   use ExUnit.Case, async: true
+  use Plug.Test
+
+  @opts StripeEventex.init([])
+
+  test "when is unknown event" do
+  	conn = StripeEventex.call(conn(:post, "/", Poison.encode!(%{event: "customer.jshj"})), @opts)
+  	assert conn.status == 200
+  	assert get_resp_header(conn, "content-type") == ["application/json; charset=utf-8"]
+  	assert conn.resp_body == Poison.encode!(%{message: "success (not subscribed)"})
+  end
 
 
   test "the truth" do
