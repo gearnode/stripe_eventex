00e482826df0d275ffe52d10850d3ee7b3dbf152
Gearnode <bfrimin@student.42.fr>
2016-06-12 17:45:50 +0200

    Add summary

--- mix.exs
+++ mix.exs
@@ -5,18 +5,33 @@ defmodule StripeEventex.Mixfile do
     [app: :stripe_eventex,
      version: "1.0.0",
      elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
      deps: deps]
   end
 
   def application do
-    [applications: [:logger, :cowboy, :plug]]
+    [applications: []]
   end
 
   defp deps do
     [{:cowboy, "~> 1.0.0"},
      {:plug, "~> 1.0"},
      {:poison, "~> 2.0"}]
+  end
+
+  defp description do
+    """
+    Stripe webhook integration for Plug.
+    """
+  end
+
+  defp package do
+    [
+     name: :stripe_eventex,
+     files: ["lib", "test", "config", "mix.exs", "README*", "LICENSE*"],
+     maintainers: ["Bryan Frimin"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/gearnode/stripe_eventex"}]
   end
 end
